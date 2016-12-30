8850e7e00e99194bab04b5dc7984f0be29155baa
iiyuk <temp>
2016-12-30 20:14:02 +0900

    ColorArrayのサイズ変更を実装

--- ColorArray.py
+++ ColorArray.py
@@ -1,43 +1,56 @@
-from PIL import Image
+"""
+二次元の色配列をいじるための関数
+"""
 
 
-class ColorArray(list):
+def blank_colorarray(height, width):
     """
-    二次元の色配列
+    [255, 255, 255](白)に初期化された横width, 縦heightの色配列を返す
     """
+    array = []
+    for _ in range(height):
+        array.append([])
+        for _ in range(width):
+            array[-1].append([255, 255, 255])
+    return array
 
-    def __init__(self, height, width):
-        """
-        [255, 255, 255](白)に初期化された横width, 縦heightの色配列を返す
-        """
-        array = []
-        for _ in range(height):
-            array.append([])
-            for _ in range(width):
-                array[-1].append([255, 255, 255])
-        super().__init__(array)
 
-    @staticmethod
-    def load_image(filename, width):
-        """
-        画像から色配列を作って返す
-        widthは出来上がる色配列のx方向の要素数
-        """
-        image = Image.open(filename)
-        original_width, original_height = image.size
-        height = width / original_width * original_height
-        diff_x = (original_width - 1) / (width - 1)
-        diff_y = (original_height - 1) / (height - 1)
+def change_size(array, width):
+    """
+    arrayを幅widthに縮小(or拡大)して返す
+    """
+    return from_source(width, len(array[0]), len(array), lambda x, y: array[int(y)][int(x)])
 
+
+def load_image(filename, width):
+    """
+    画像から色配列を作って返す
+    widthは出来上がる色配列の幅
+    """
+    from PIL import Image
+
+    image = Image.open(filename)
+    w, h = image.size
+    return from_source(width, w, h, lambda x, y: image.getpixel((int(x), int(y))))
+
+
+def from_source(width, source_width, source_height, sampler):
+    """
+    samplerを使って幅widthの色配列を作って返す
+    sampler: [0, source_width] * [0, source_height] -> RGB
+    """
+    height = width / source_width * source_height
+    diff_x = (source_width - 1) / (width - 1)
+    diff_y = (source_height - 1) / (height - 1)
+
+    x = 0
+    y = 0
+    array = []
+    while y <= source_height:
+        array.append([])
         x = 0
-        y = 0
-        array = []
-        while y <= original_height:
-            array.append([])
-            x = 0
-            while x <= original_width:
-                color = image.getpixel((int(x), int(y)))
-                array[-1].append(color)
-                x += diff_x
-            y += diff_y
-        return array
+        while x <= source_width:
+            array[-1].append(sampler(x, y))
+            x += diff_x
+        y += diff_y
+    return array
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import argparse
 import os
 from Figure import Point, Line, Circle
-from ColorArray import ColorArray
+import ColorArray
 from ColorArrayPainter import ColorArrayPainter
 from ExcelArrayPrinter import ExcelArrayPrinter
 from ShellArrayPrinter import ShellArrayPrinter
@@ -17,26 +17,22 @@ def make_parser():
     parser = argparse.ArgumentParser(description="Make excel from image.")
     parser.add_argument(dest="filename", action="store",
                         help="Configure path to the image.")
-    parser.add_argument("-n", dest="width", default=50, action="store",
+    parser.add_argument("-n", dest="width", default=200, action="store",
                         help="Configure the width of generationg image.")
     return parser
 
 
-def prompt():
+def test_figure(args):
     """
-    対話処理など
+    図形描画のテスト
     """
-    arg = make_parser().parse_args()
     painter = ColorArrayPainter()
-
-    array = ColorArray(100, 100)
-    # array = ColorArray.load_image(arg.filename, arg.width)
-    filename = os.path.splitext(arg.filename)[0]
+    array = ColorArray.blank_colorarray(args.width, args.width)
 
     p = [Point(0, 0, [0, 0, 0]),
-         Point(99, 0, [255, 255, 0]),
-         Point(99, 99, [255, 0, 255]),
-         Point(0, 99, [0, 255, 255])]
+         Point(args.width, 0, [255, 255, 0]),
+         Point(args.width, args.width, [255, 0, 255]),
+         Point(0, args.width, [0, 255, 255])]
 
     painter.draw(array, Line(p[0], p[1]))
     painter.draw(array, Line(p[1], p[2]))
@@ -45,12 +41,42 @@ def prompt():
     painter.draw(array, Line(p[0], p[2]))
     painter.draw(array, Line(p[1], p[3]))
 
-    circle = Circle(Point(50, 50, [0, 255, 0]), 50)
+    circle = Circle(Point(args.width / 2, args.width /
+                          2, [0, 255, 0]), args.width / 2)
     painter.draw(array, circle)
+
+    ExcelArrayPrinter("figure").print(array)
+    ShellArrayPrinter("figure").print(array)
+    HtmlArrayPrinter("figure").print(array)
+    print("figure test ended.")
+
+
+def test_image(args):
+    """
+    画像をロードする関数のテスト
+    """
+    array = ColorArray.load_image(args.filename, args.width)
+    filename = os.path.splitext(args.filename)[0]
 
     ExcelArrayPrinter(filename).print(array)
     ShellArrayPrinter(filename).print(array)
     HtmlArrayPrinter(filename).print(array)
-    print("ended.")
+
+    array = ColorArray.change_size(array, 100)
+    HtmlArrayPrinter(filename + "_resized").print(array)
+    ExcelArrayPrinter(filename + "_resized").print(array)
+    ShellArrayPrinter(filename + "_resized").print(array)
+
+    print("image test ended.")
+
+
+def prompt():
+    """
+    対話処理など
+    """
+    args = make_parser().parse_args()
+    test_figure(args)
+    test_image(args)
+    print("program ended.")
 
 prompt()
