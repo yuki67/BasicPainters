969fc397ba9ac4d69ac3b45293f57c31481b2917
iiyuk <temp>
2017-01-03 17:57:16 +0900

    テストを統一

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import tkinter
 from math import sin, cos, pi
 from Figure import Point, Line, Circle, Ellipse, Diamond, Polygon
-import ColorArray
+from ColorArray import colorarray_blank, colorarray_load_image, colorarray_resize
 from TkPainter import TkPainter
 from ColorArrayPainter import ColorArrayPainter
 from ExcelArrayPrinter import ExcelArrayPrinter
@@ -29,14 +29,14 @@ def test_image(args):
     """
     画像をロードする関数のテスト
     """
-    array = ColorArray.load_image(args.filename, args.width)
+    array = colorarray_load_image(args.filename, args.width)
     filename = os.path.splitext(args.filename)[0]
 
     ExcelArrayPrinter(filename).print(array)
     ShellArrayPrinter(filename).print(array)
     HtmlArrayPrinter(filename).print(array)
 
-    array = ColorArray.change_size(array, 100)
+    array = colorarray_load_image(array, 100)
     HtmlArrayPrinter(filename + "_resized").print(array)
     ExcelArrayPrinter(filename + "_resized").print(array)
     ShellArrayPrinter(filename + "_resized").print(array)
@@ -44,15 +44,15 @@ def test_image(args):
     print("image test ended.")
 
 
-def test_figure(canvas, painter):
+def test_figure(canvas, painter, width, height):
     """
     図形描画のテスト
     """
     p = [Point(1, 1, [0, 0, 0]),
-         Point(canvas.width - 1, 1, [255, 255, 0]),
-         Point(canvas.width - 1,
-               canvas.height - 1, [255, 0, 255]),
-         Point(1, canvas.height - 1, [0, 255, 255])]
+         Point(width - 1, 1, [255, 255, 0]),
+         Point(width - 1,
+               height - 1, [255, 0, 255]),
+         Point(1, height - 1, [0, 255, 255])]
     """
     Line, Polygon
     """
@@ -63,26 +63,31 @@ def test_figure(canvas, painter):
     """
     Eillipe,Circle
     """
-    center = Point(canvas.width / 2,
-                   canvas.height / 2, [0, 255, 0])
-    painter.draw(canvas, Circle(center, canvas.width / 4))
+    center = Point(width / 2,
+                   height / 2, [0, 255, 0])
+    painter.draw(canvas, Circle(center, width / 4))
     center.rgb = [0, 0, 255]
     painter.draw(canvas, Ellipse(
-        center, canvas.width / 4, canvas.width / 2))
+        center, width / 4, width / 2))
     center.rgb = [255, 0, 0]
     painter.draw(canvas, Ellipse(
-        center, canvas.width / 2, canvas.width / 4))
+        center, width / 2, width / 4))
 
     """
     Diamond
     """
     painter.draw(canvas,
                  Diamond(center,
-                         canvas.width / 2, 16,
+                         width / 2, 16,
                          lambda t: [128 + 127 * sin(2 * pi * t),
                                     128 + 127 * cos(2 * pi * t),
                                     128 + 127 * sin(2 * pi * t) * cos(2 * pi * t)]),)
-    canvas.place(x=5, y=5)
+
+
+def test_array_printer(canvas, filename):
+    ExcelArrayPrinter(filename).print(canvas)
+    ShellArrayPrinter(filename).print(canvas)
+    HtmlArrayPrinter(filename).print(canvas)
 
 
 def setup_tk(width, height):
@@ -93,8 +98,6 @@ def setup_tk(width, height):
     root.title("Tk Painter")
     root.geometry("%dx%d+%d+%d" % (width + 10, height + 10, 256, 0))
     canvas = tkinter.Canvas(root, width=width, height=height)
-    canvas.width = width
-    canvas.height = height
     return root, canvas
 
 
@@ -105,11 +108,28 @@ def prompt():
     args = make_parser().parse_args()
 
     """
+    ArrayPainter
+    """
+    width, height = 100, 100
+    canvas = colorarray_blank(width, height)
+    test_figure(canvas, ColorArrayPainter(), width, height)
+    test_array_printer(canvas, "figures")
+
+    canvas = colorarray_load_image(args.filename, width)
+    filename = os.path.splitext(args.filename)[0]
+    test_array_printer(canvas, filename)
+    test_array_printer(colorarray_resize(canvas, 50), filename)
+
+    """
     TkPainter
     """
+    width, height = 512, 512
     root, canvas = setup_tk(512, 512)
-    test_figure(canvas, TkPainter())
+    test_figure(canvas, TkPainter(), width, height)
+    canvas.place(x=5, y=5)
+
     root.mainloop()
+
     # test_figure(args)
     # test_image(args)
     print("program ended.")
