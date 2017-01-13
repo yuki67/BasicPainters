cf189da5ab7961dd8ecc14e9353055c2f3058e15
yuki <123syokutaku@gmail.com>
2017-01-13 13:44:08 +0900

    図形を描くデモを追加

--- DrawFigure.py
+++ DrawFigure.py
@@ -12,38 +12,6 @@ from ShellArrayPrinter import ShellArrayPrinter
 from HtmlArrayPrinter import HtmlArrayPrinter
 
 
-def make_parser():
-    """
-    パーサーを作って、パーサーを返す。
-    返り値に直接parse_arg()することが前提になっている。
-    """
-    parser = argparse.ArgumentParser(description="Make excel from image.")
-    parser.add_argument(dest="filename", action="store",
-                        help="Configure path to the image.")
-    parser.add_argument("-n", dest="width", default=200, action="store",
-                        help="Configure the width of generationg image.")
-    return parser
-
-
-def test_image(args):
-    """
-    画像をロードする関数のテスト
-    """
-    array = colorarray_load_image(args.filename, args.width)
-    filename = os.path.splitext(args.filename)[0]
-
-    ExcelArrayPrinter(filename).print(array)
-    ShellArrayPrinter(filename).print(array)
-    HtmlArrayPrinter(filename).print(array)
-
-    array = colorarray_load_image(array, 100)
-    HtmlArrayPrinter(filename + "_resized").print(array)
-    ExcelArrayPrinter(filename + "_resized").print(array)
-    ShellArrayPrinter(filename + "_resized").print(array)
-
-    print("image test ended.")
-
-
 def test_figure(canvas, painter, width, height):
     """
     図形描画のテスト
@@ -84,10 +52,11 @@ def test_figure(canvas, painter, width, height):
                                     128 + 127 * sin(2 * pi * t) * cos(2 * pi * t)]),)
 
 
-def test_array_printer(canvas, filename):
-    ExcelArrayPrinter(filename).print(canvas)
-    ShellArrayPrinter(filename).print(canvas)
-    HtmlArrayPrinter(filename).print(canvas)
+def draw_array_printer(array, filename):
+    """ arrayPainterを使ってarrayを描く """
+    ExcelArrayPrinter(filename).print(array)
+    ShellArrayPrinter(filename).print(array)
+    HtmlArrayPrinter(filename).print(array)
 
 
 def setup_tk(width, height):
@@ -105,7 +74,6 @@ def prompt():
     """
     対話処理など
     """
-    args = make_parser().parse_args()
 
     """
     ArrayPainter
@@ -113,12 +81,10 @@ def prompt():
     width, height = 100, 100
     canvas = colorarray_blank(width, height)
     test_figure(canvas, ColorArrayPainter(), width, height)
-    test_array_printer(canvas, "figures")
 
-    canvas = colorarray_load_image(args.filename, width)
-    filename = os.path.splitext(args.filename)[0]
-    test_array_printer(canvas, filename)
-    test_array_printer(colorarray_resize(canvas, 50), filename)
+    if not os.path.exists("./figures"):
+        os.mkdir("figures")
+    draw_array_printer(canvas, os.path.join("figures", "figure"))
 
     """
     TkPainter
@@ -127,11 +93,7 @@ def prompt():
     root, canvas = setup_tk(512, 512)
     test_figure(canvas, TkPainter(), width, height)
     canvas.place(x=5, y=5)
-
     root.mainloop()
-
-    # test_figure(args)
-    # test_image(args)
     print("program ended.")
 
 prompt()
