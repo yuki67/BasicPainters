c6460cf5f17df56962c64a463ed5daa3710e4bba
yuki <123syokutaku@gmail.com>
2017-02-08 13:26:06 +0900

    Painter.drawを簡略化

--- Painter/Painter.py
+++ Painter/Painter.py
@@ -15,10 +15,11 @@ class Painter(object):
         """ canvasにpointを描画する """
         assert False, "Override me!"
 
+    def split_and_draw(self, figure: Figure) -> None:
+        """ figureを分解して描く """
+        for sub_figure in figure:
+            self.draw(sub_figure)
+
     def draw(self, figure: Figure) -> None:
         """ canvasにfigureを描く """
-        if self.draw_functions.get(type(figure)):
-            self.draw_functions[type(figure)](figure)
-        else:
-            for p in figure:
-                self.draw(p)
+        self.draw_functions.get(type(figure), self.split_and_draw)(figure)
